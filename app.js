82d6c4d8e78eb529b121f33f2826346d715e2b68
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-08-22 16:16:13 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,68 +1,18 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+const https = require('https');
+
+https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
+  let data = '';
+
+  // A chunk of data has been received.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data).explanation);
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
-
-// // moduels
-
-
-var mysql = require('mysql');
-var bodyParser = require('bodyParser')
-
-$(document).ready(() => {
-
-    $.ajax({
-        url: "http://localhost:9000/list", 
-        method: 'GET',
-        success: function(response){
-            if(response.rows.length > 0){
-                for(let index = 0; index < response.rows.length; index++) {
-                    var newRow = $("<tr>");
-                    var cols = "";
-                    var firstname = '';
-                    var lastname = '';
-                    var gender = '';
-                    cols += '<td> '+ response.rows[index].firstname +'</td>';
-                    cols += '<td> '+ response.rows[index].lastname +'</td>';
-                    cols += '<td> '+ response.rows[index].gender+'</td>';                
-                    newRow.append(cols);
-                    $("#tableData .tbody").append(newRow);
-                }  
-    
-            }
-        }
-    })
-    })
-
-// // 
-// app.use(express.static(__dirname + '/public'));
-// app.use(bodyParser.urlencoded({extended: false}));
-
-// // connect to database
-// var con = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "1234",
-//     database: "BathBombs_DB"
-// });
-
-// // routes
-// app.get('/', function(request, response){
-//     console.log('GET request received at /') 
-//     con.query("SELECT * FROM customers", function (err, result) {
-//         if (err) throw err;
-//         else{
-//             response.send(result)
-//         }
-
-//     });
-// });
-
-// // listen for trafic and display on localhost:9000
-// app.listen(9000, function () {
-//     console.log('Connected to port 9000');
-// });
