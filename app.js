7eedd1fd2ec2746eedfa37484466947a3fed4d6d
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-08-23 09:39:16 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
-const https = require('https');
+var express = require('express');
+var path = require('path');
+var app = express();
 
-https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
-  let data = '';
+app.use(express.static(path.join(__dirname, 'view')));
 
-  // A chunk of data has been received.
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
-
-  // The whole response has been received. Print out the result.
-  resp.on('end', () => {
-    console.log(JSON.parse(data).explanation);
-  });
-
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
+app.listen(process.env.PORT || 8000, '0.0.0.0', function() {});
+module.exports = app;
